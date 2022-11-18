d1ec17fab40b2266891d136a7a570d6a1c68d65d
DIBSTER <76603072+DIBSTERYT@users.noreply.github.com>
2022-11-18 18:20:38 -0500

    feat: Automatic GitHub pulling

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 import favicon from 'serve-favicon';
 import { join } from 'path';
+import child_process from 'child_process';
 
 const app: Express = express();
 const port = process.env.SERVER_PORT || 3000;
@@ -11,6 +12,24 @@ app.use('/public', express.static(join(__dirname, "public")));
 app.get("/", (req: Request, res: Response) => {
     res.sendFile(__dirname + '/index.html');
 });
+
+ function GetFromGitHub(){
+        child_process.exec(`git pull`, (error, stdout) => {
+            let response = (error || stdout);
+            if (!error) {
+                if (response.includes("Already up to date.")) {
+                } else {
+                    setTimeout(() => {
+                        process.exit();
+                    }, 1000);
+                };
+            };
+        });
+};
+
+setInterval(() => {
+    GetFromGitHub();
+}, 30 * 1000);
 
 app.listen(port, () => {
     console.clear();
